870c61a4116e740d59399f9a5345ab278de43ab7
fasih_ek <fasihedakkudi@gmail.com>
2023-11-16 09:40:10 +0530

    remove file

--- server/routes/employeeRoutes.js
+++
@@ -1,9 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-
-// router.route("/").get((req, res) => {
-//   res.json({ message: "Get all Employees" });
-// });
-
-
-// module.exports = router;
